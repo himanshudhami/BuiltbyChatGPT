5ce7d34ae2aaf6ce574b28330563002a391363f5
Himanshu <himanshudhami@gmail.com>
2023-02-24 02:09:17 +0100

    chatgpt gave up on tailwind so a manual hack here

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import "./styles/tailwind-pre-build.css";
 import Dashboard from './Dashboard';
 import AddCompany from './Companies';
 import AddCustomer from './Customers';
